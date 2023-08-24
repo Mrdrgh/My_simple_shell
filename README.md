037feb6d238db786ecb2b8598a9bbf32c7f6352e
Mrdrgh <mrdrgh2003@gmail.com>
2023-08-24 12:44:10 +0100

    added all files from local storage

---
+++ README.md
@@ -0,0 +1,26 @@
+# Simple Shell Program Documentation
+
+The `simple_shell` is a minimalistic command-line interpreter program that allows users to execute commands in interactive mode or by using the pipe operator.
+
+## Author
+- Mrdrgh
+
+## Features
+
+### Interactive Mode
+The `simple_shell` supports interactive mode, where users can input commands directly in the terminal and receive output accordingly.
+
+### Piped Command Usage
+Users can also use the pipe operator (`|`) to feed the output of one command as input to another command. For example:
+
+```sh
+echo "command" | program_name
+```
+
+### Non Piped Command Usage
+or we can use the program in interactive mode, just like sh or born_again_sh to start using it run :
+```./hsh
+$ <command> 
+<prompt>
+$ ...
+```
---
+++ shell.h
@@ -0,0 +1,70 @@
+#ifndef SHELL_H
+#define SHELL_H
+
+#include <stdio.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/wait.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <stdbool.h>
+#include <errno.h>
+#include <limits.h>
+
+/* string_funtions_1 */
+int _putchar(char);
+int _puts(char *);
+char *_strcat(char *, char *);
+char *_strncat(char *, char *, int);
+int _strlen(char *);
+
+/* string_funtions_2 */
+int _strcmp(char *, char *);
+char **splitstr(char *);
+char *_strcpy(char *, char *);
+char *_strdup(char *);
+int _strncmp(char *, char *, int);
+
+/* string_functions_3*/
+char ***split_str_prime(char *);
+
+/* executionners */
+void exec_non_interactive(char **envp);
+void executionner_prime(char **, char *, int);
+void executionner(char *, int, char **, char **);
+void exec1(char *, char **, char **);
+void exec2(char *);
+void exec3(char *, char **);
+void exec4(char **, char **);
+/* _realloc */
+void *_realloc(void *, unsigned int, unsigned int);
+
+/* path_handling_functions */
+char *_getenv(char *, char **);
+char *get_command(char *, char **);
+void print_env(char **);
+int change_dir(char **, char **);
+
+/* checker */
+int checker(char *, char **);
+
+/* _strtok */
+char *_strtok_r(char *, char *, char **);
+
+/* _getline*/
+ssize_t non_interactive_getline(char **lineptr, size_t *n, FILE *stream);
+ssize_t _getline(char **lineptr, size_t *n, FILE *stream);
+int _getchar(void);
+
+/* env */
+int _setenv(char *, char *, bool);
+int _unsetenv(char *);
+
+/* is_cmd */
+int is_command(char *cmd, char **envp);
+
+/* the enviroment pointer*/
+extern char **environ;
+#endif
